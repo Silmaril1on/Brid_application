fd83b8c172aa74991cd3af0cc6788401ec6db7f2
tsotne932 <tsotne.maisa@gmail.com>
2024-07-03 17:21:55 +0400

    navigation fix

--- src/pages/projectpage/ProjectButtons.js
+++ src/pages/projectpage/ProjectButtons.js
@@ -6,37 +6,55 @@ import projectsData from "../../database/projectsData";
 
 function ProjectButtons({ details }) {
   const projectLinks = projectsData.map((item) => item.link);
-  const [index, setIndex] = useState(details.id);
-  const [link, setLink] = useState(projectLinks[index]);
-
-  const previous = () => {
-    if (index === 0) {
-      setIndex(projectsData.length - 1);
-    } else {
-      setIndex((prev) => prev - 1);
-    }
-  };
-
-  const next = () => {
-    if (index === projectsData.length - 1) {
-      setIndex(0);
-    } else {
-      setIndex((prev) => prev + 1);
-    }
-  };
+  // const [index, setIndex] = useState(details.id);
+  // const [link, setLink] = useState(projectLinks[index]);
+  const [nextLink, setNextlink] = useState();
+  const [prevLink, setPrevLink] = useState();
 
   useEffect(() => {
-    setLink(projectLinks[index]);
-  }, [index, projectLinks]);
+    if (details.id === 0) {
+      setPrevLink(projectLinks[projectsData.length - 1])
 
-  console.log(index, "index");
+    } else {
+      setPrevLink(projectLinks[details.id - 1])
+    }
+
+    if (details.id == projectLinks.length - 1) {
+      setNextlink(projectLinks[0])
+    } else {
+      setNextlink(projectLinks[details.id + 1])
+    }
+
+  }, [details])
+
+  // const previous = () => {
+  //   if (index === 0) {
+  //     setIndex(projectsData.length - 1);
+  //   } else {
+  //     setIndex((prev) => prev - 1);
+  //   }
+  // };
+
+  // const next = () => {
+  //   if (index === projectsData.length - 1) {
+  //     setIndex(0);
+  //   } else {
+  //     setIndex((prev) => prev + 1);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   setLink(projectLinks[index]);
+  // }, [index, projectLinks]);
+
+  // console.log(index, "index");
 
   return (
     <div className="w-full py-4 flex justify-between px-5">
-      <NavLink onClick={previous} to={link} className="center">
+      <NavLink to={prevLink} className="center">
         <LeftButton />
       </NavLink>
-      <NavLink onClick={next} to={link} className="center">
+      <NavLink to={nextLink} className="center">
         <RightButton />
       </NavLink>
     </div>
